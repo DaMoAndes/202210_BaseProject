9a807d8313afb8169cbc867b19a24647becff312
David Morales Aguilar <d.moralesa2@uniandes.edu.co>
2022-04-30 11:05:43 -0500

    Creacion módulo cafe; componente, componente-detalle, servicio, endpoint-environment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CafeComponent } from './cafe/cafe.component';
+import { CafeListComponent } from './cafe/cafe-list/cafe-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CafeComponent,
+    CafeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
+  exports: [CafeListComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
