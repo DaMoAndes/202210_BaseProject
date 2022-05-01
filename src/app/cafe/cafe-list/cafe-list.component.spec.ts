1dfbc3184b56c385e15d0d0cafc2f4d5a6c402bb
David Morales Aguilar <d.moralesa2@uniandes.edu.co>
2022-04-30 22:17:32 -0500

    Creación de pruebas con unm listado de 3 tipos de cafes

--- src/app/cafe/cafe-list/cafe-list.component.spec.ts
+++ src/app/cafe/cafe-list/cafe-list.component.spec.ts
@@ -1,14 +1,20 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { Cafe } from '../cafe';
+import { faker } from '@faker-js/faker'
 import { CafeListComponent } from './cafe-list.component';
+import { By } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 describe('CafeDetailComponent', () => {
   let component: CafeListComponent;
   let fixture: ComponentFixture<CafeListComponent>;
-
+  let debug: DebugElement;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CafeListComponent ]
+      declarations: [ CafeListComponent ],
+      imports:[HttpClientModule]
+
     })
     .compileComponents();
   });
@@ -16,10 +22,45 @@ describe('CafeDetailComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(CafeListComponent);
     component = fixture.componentInstance;
+    component.cafes = [
+      new Cafe(
+          faker.datatype.number(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.lorem.sentence(),
+          faker.datatype.number(),
+          faker.image.imageUrl()
+      ),
+      new Cafe(
+        faker.datatype.number(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.lorem.sentence(),
+        faker.datatype.number(),
+        faker.image.imageUrl()
+    ),
+    new Cafe(
+      faker.datatype.number(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.lorem.sentence(),
+      faker.datatype.number(),
+      faker.image.imageUrl()
+  ),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('Se verifica la tabla con tres filas más el encabaezado', () => {
+    console.log(debug);
+
+    const filas  = debug.queryAll(By.css('tbody tr'));
+    const encabezado = debug.queryAll(By.css('thead tr'))
+    expect(filas.length + encabezado.length).toBe((4))
+
   });
 });
